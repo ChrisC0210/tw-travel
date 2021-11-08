391051d6a84ad2ff9f5bcef2b3638746b43fa124
Chris Chen <chiandeye@gmail.com>
2021-11-09 02:10:49 +0800

    Add vue setting

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="container">
+    <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@import "bootstrap";
+</style>
