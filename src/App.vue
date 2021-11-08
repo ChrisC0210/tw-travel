b02590890706c90dce5c72edef5ec81140b27190
Chris Chen <chiandeye@gmail.com>
2021-11-09 03:06:58 +0800

    (1)Add :logo, views, App.vue (2)Edit : index.js

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
 <div class="container">
     <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/" class="me-4">
+    <img src="./assets/tw-travel-logo.svg" alt="">
+    </router-link>
+    <router-link to="/viewer" class="me-4">找景點</router-link>
+    <router-link to="/about" class="me-4">找景點</router-link>
+    <router-link to="/about" class="me-4">找景點</router-link>
   </div>
 </div>
   <router-view/>
