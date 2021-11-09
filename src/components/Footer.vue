23e0aea8426b3bc0b74f657d383551c378842bc1
Chris Chen <chiandeye@gmail.com>
2021-11-09 23:44:53 +0800

    (1)Add Carousel,  vue3-carousel (2)Edit footer, home

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    <div class="container-fluid text-center bg-light p-5 fixed-bottom">
+    <div class="container-fluid text-center bg-light fixed-bottom">
     <p>Copyright © Christina Chen. All Rights Reserved.</p>
     </div>
   </div>
